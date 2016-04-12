3ba1e70f9cc35ca10a5cb12896fa8ff2f0e4c961
SaitouRedEyes <zeca.saitou@gmail.com>
2016-04-12 17:13:35 -0300

    Basic PHP server example.
    This projects show the basic of a php server, where receive a http GET
    or POST connection, and redirect to webservices that access a MySQL
    database to get and set data.

---
+++ requests.php
@@ -0,0 +1,34 @@
+<?php	
+include "services.php";
+				
+	if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET["sID"]) && $_GET["sID"] != "")
+	{ 
+		echo RedirectToServices($_GET);		
+	}
+	else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["sID"]) && $_POST["sID"] != "")
+	{		
+		echo RedirectToServices($_POST); 
+	}	
+	else echo "<h1>Método/Serviço requerido desconhecido!!</h1>";		
+	
+	
+	function RedirectToServices($methodRequest)
+	{
+		$GetHighScore = 0;
+		$SetHighscore = 1;
+				
+		$services = Services::GetInstance();
+		$errorMessage = "<h1>Problemas com os parametros do request!!</h1>";
+		
+		switch ($methodRequest["sID"])
+		{
+			case $GetHighScore: return $services->GetHighscore(); break;
+				
+			case $SetHighscore:
+				if(isset($methodRequest["value"]) && $methodRequest["value"] != "") return $services->SetHighscore($methodRequest["value"]);
+				else return $errorMessage; break;
+				
+			default: return $errorMessage;
+		}
+	}
+?>
---
+++ services.php
@@ -0,0 +1,63 @@
+<?PHP
+
+class Services
+{
+	const HOST = "localhost";
+	const USER = "root";
+	const PASS = "vertrigo";
+	const DB = "arkanoid";
+	
+	private static $instance;
+		
+	private function __construct() 
+	{}
+	
+	public static function GetInstance() 
+	{	
+		if(!self::$instance) self::$instance = new self();		
+	
+		return self::$instance;	
+	}	
+	
+	/**
+	 * Get Highscore
+	 * @param string $login
+	 * @param string $pass
+	 */
+	public function GetHighscore()
+	{
+		$con = mysqli_connect(self::HOST, self::USER, self::PASS, self::DB);
+		
+		if (mysqli_connect_errno()) return false;
+
+		$result = mysqli_query($con, "SELECT value FROM highscore");
+		
+		if(mysqli_num_rows($result) > 0) $row = mysqli_fetch_assoc($result); 
+		else $row = "";		
+		
+		mysqli_close($con);
+		
+		return $row === "" ? false : $row["value"];
+	}
+	
+	public function SetHighscore($highscore)
+	{
+		$con = mysqli_connect(self::HOST, self::USER, self::PASS, self::DB);
+	
+		if (mysqli_connect_errno()) return false;
+	
+		mysqli_query($con, "UPDATE highscore SET value = '$highscore' WHERE id = 0");
+		
+		mysqli_close($con);
+	
+		return true;
+	}
+}
+?>
+
+
+
+
+
+
+
